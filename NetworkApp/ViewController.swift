6d07837bb43539ce9450f3b05c00c7375f257c11
Leticia <leticiaspeda@gmail.com>
2024-01-25 11:46:53 -0300

    Initial Commit

---
+++ NetworkApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  NetworkApp
+//
+//  Created by Leticia Speda on 25/01/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
