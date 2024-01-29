99c1de3238819575ae1e5b741cdd2a7c91681961
Leticia <leticiaspeda@gmail.com>
2024-01-29 11:43:32 -0300

    refactor: ajustado conforme a internet

--- NetworkApp/Home/Service/HomeService.swift
+++ NetworkApp/Home/Service/HomeService.swift
@@ -3,34 +3,36 @@ import Foundation
 final class HomeService {
     
     func getPersonList(completion: @escaping (Result<PersonList, Error>)-> Void) {
-        let urlString = URL(string: "https://run.mocky.io/v3/65af81de-e83e-4cce-b751-f239388fdff5")
-        
-        guard let url = urlString else { return }
+        guard let url = URL(string: "https://run.mocky.io/v3/65af81de-e83e-4cce-b751-f239388fdff5") else {
+            completion(.failure(NSError(domain: "Invalid URL", code: 0, userInfo: nil)))
+            return
+        }
         
         let task = URLSession.shared.dataTask(with: url) { data, response, error in
-            if let error {
-                print("ERROR \(#function) Detalhe do erro: \(error.localizedDescription)")
-                completion(.failure(error.localizedDescription as! Error))
-                return
-            }
-            
-            guard let data else { 
-                completion(.failure(error?.localizedDescription as! Error))
+            guard let data = data, error == nil else {
+                if let error = error {
+                    print("Error in \(#function): \(error.localizedDescription)")
+                    completion(.failure(error))
+                } else {
+                    completion(.failure(NSError(domain: "No data received", code: 0, userInfo: nil)))
+                }
                 return
             }
             
             guard let response = response as? HTTPURLResponse, response.statusCode == 200 else {
-                completion(.failure(error?.localizedDescription as! Error))
+                let statusCode = (response as? HTTPURLResponse)?.statusCode ?? 0
+                print("Invalid response in \(#function): Status Code \(statusCode)")
+                completion(.failure(NSError(domain: "Invalid response", code: statusCode, userInfo: nil)))
                 return
             }
             
             do {
                 let personList: PersonList = try JSONDecoder().decode(PersonList.self, from: data)
-                print("Success -> \(#function)")
+                print("Success in \(#function)")
                 completion(.success(personList))
             } catch  {
-                print("Error -> \(#function)")
-                completion(.failure(error.localizedDescription as! Error))
+                print("Error decoding JSON in \(error.localizedDescription)")
+                completion(.failure(error))
             }
         }
         task.resume()
