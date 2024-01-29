c0256cb3ae19fef60540603e6f509668fecd568a
Leticia <leticiaspeda@gmail.com>
2024-01-29 11:44:22 -0300

    feat: colocado o swift case para sucesso ou falha

--- NetworkApp/Home/ViewModel/HomeViewModel.swift
+++ NetworkApp/Home/ViewModel/HomeViewModel.swift
@@ -1,7 +1,16 @@
 import UIKit
 
 final class HomeViewModel {
+    var service =  HomeService()
+    
     func fetchRequest() { // metodo para disparar reqscao
-        
+        service.getPersonList { result in
+            switch result {
+            case .success(let success):
+                print(success)
+            case .failure(let failure):
+                print(failure)
+            }
+        }
     }
 }
