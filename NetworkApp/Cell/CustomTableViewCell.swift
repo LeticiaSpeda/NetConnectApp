2459c16b96c96378f14944661440c0954a8cfc7b
Leticia <leticiaspeda@gmail.com>
2024-02-08 10:32:08 -0300

    feat: adicionado novas pastas e arquivos

---
+++ NetworkApp/Cell/CustomTableViewCell.swift
@@ -0,0 +1,8 @@
+//
+//  CustomTableViewCell.swift
+//  NetworkApp
+//
+//  Created by Leticia Speda on 08/02/24.
+//
+
+import Foundation
---
+++ NetworkApp/CustomTableViewCellScreen.swift
@@ -0,0 +1,8 @@
+//
+//  CustomTableViewCellScreen.swift
+//  NetworkApp
+//
+//  Created by Leticia Speda on 08/02/24.
+//
+
+import Foundation
