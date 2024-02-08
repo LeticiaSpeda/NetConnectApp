4500253d44a60eb0f939f01e51d35a7be357a954
Leticia <leticiaspeda@gmail.com>
2024-02-08 10:33:24 -0300

    feat: criado viewController onde a view passa a ser a HomeScreen

--- NetworkApp/Home/HomeViewController.swift
+++ NetworkApp/Home/HomeViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  HomeViewController.swift
 //  NetworkApp
 //
 //  Created by Leticia Speda on 25/01/24.
@@ -9,11 +9,49 @@ import UIKit
 
 final class HomeViewController: UIViewController {
     
-    var viewModel = HomeViewModel()
-
+    var screen =  HomeScreen()
+    private let viewModel = HomeViewModel()
+    
+    override func loadView() {
+        super.loadView()
+        //        self.screen = HomeScreen()
+        self.view = screen
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .yellow
         viewModel.fetchRequest()
+        viewModel.delegate(delegate: self)
+    }
+}
+
+extension HomeViewController: HomeViewModelDelegate {
+    func success() {
+        screen.configTableViewProtocols(delegate: self, dataSource: self)
+        screen.tableView.reloadData()
+    }
+    
+    func error(message: String) {
+        let alert = UIAlertController(title: "Houve um problema", message: message, preferredStyle: .alert)
+        let addAlert = UIAlertAction(title: "ok", style: .cancel)
+        alert.addAction(addAlert)
+        present(alert, animated: true)
+    }
+}
+
+extension HomeViewController: UITableViewDelegate, UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        viewModel.numberOfRowsInSection
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: CustomTableViewCell.identifier, for: indexPath) as? CustomTableViewCell
+        cell?.setpHomeCell(data: viewModel.loadCurrentPeople(indexPath: indexPath))
+        return cell ?? UITableViewCell()
+    }
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 100
     }
 }
