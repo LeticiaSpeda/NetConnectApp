d61ff446f63895e8c3d035463b83365547834c02
Leticia <leticiaspeda@gmail.com>
2024-02-08 10:37:44 -0300

    feat: criado tela da view

--- NetworkApp/Home/HomeScreen.swift
+++ NetworkApp/Home/HomeScreen.swift
@@ -1,8 +1,42 @@
-//
-//  HomeScreen.swift
-//  NetworkApp
-//
-//  Created by Leticia Speda on 08/02/24.
-//
+import UIKit
 
-import Foundation
+final class HomeScreen: UIView, ViewCode {
+    
+    lazy var tableView: UITableView = {
+        let tableView = UITableView()
+        tableView.register(CustomTableViewCell.self, forCellReuseIdentifier: CustomTableViewCell.identifier)
+        tableView.enableViewCode()
+        return tableView
+    }()
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        commonInit()
+    }
+    
+    func configTableViewProtocols(delegate: UITableViewDelegate, dataSource: UITableViewDataSource) {
+        tableView.delegate = delegate
+        tableView.dataSource = dataSource
+    }
+    
+    @available(*, unavailable)
+    required init?(coder: NSCoder) { nil }
+    
+    func setupHierarchy() {
+        addSubview(tableView)
+    }
+    
+    func setupConstraints() {
+        NSLayoutConstraint.activate([
+            tableView.topAnchor.constraint(equalTo: safeAreaLayoutGuide.topAnchor),
+            tableView.leadingAnchor.constraint(equalTo: leadingAnchor),
+            tableView.trailingAnchor.constraint(equalTo: trailingAnchor),
+            tableView.bottomAnchor.constraint(equalTo: safeAreaLayoutGuide.bottomAnchor),
+        ])
+    }
+    
+    func setupStyle() {
+        backgroundColor = .white
+    }
+}
+
