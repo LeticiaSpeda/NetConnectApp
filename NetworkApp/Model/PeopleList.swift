b46eb27b6848fa9cb79a704ca337448a8e8d681a
Leticia <leticiaspeda@gmail.com>
2024-02-08 10:35:41 -0300

    fix: ajustado nome pois a api nao possui N maiusculo

--- NetworkApp/Model/PeopleList.swift
+++ NetworkApp/Model/PeopleList.swift
@@ -1,10 +1,5 @@
-
-//struct PeopleList: Codable {
-//    var people: [People]
-//}
-
 struct People: Codable {
     var name: String
-    var userName: String
+    var username: String
     var phone: String
 }
