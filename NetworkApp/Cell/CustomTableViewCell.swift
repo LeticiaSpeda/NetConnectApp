7d6d8568bb95a35eeb162204fe9c79cd8d39b752
Leticia <leticiaspeda@gmail.com>
2024-02-08 10:39:25 -0300

    feat: criado tableviewCell e adicionado a tableViewCellScreen a ela

--- NetworkApp/Cell/CustomTableViewCell.swift
+++ NetworkApp/Cell/CustomTableViewCell.swift
@@ -1,8 +1,45 @@
-//
-//  CustomTableViewCell.swift
-//  NetworkApp
-//
-//  Created by Leticia Speda on 08/02/24.
-//
+import UIKit
 
-import Foundation
+final class CustomTableViewCell: UITableViewCell, ViewCode {
+    static let identifier = String(describing: CustomTableViewCell.self)
+    
+    // MARK -  Properties
+    lazy var screen: CustomTableViewCellScreen = {
+        let view = CustomTableViewCellScreen()
+        view.enableViewCode()
+        return view
+    }()
+    
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+        commonInit()
+    }
+    
+    @available(*, unavailable)
+    required init?(coder: NSCoder) { nil }
+    
+    
+    func setpHomeCell(data: People) {
+        screen.nameLabel.text = "Nome: \(data.name)"
+        screen.surnameLabel.text = "UserName: \(data.username)"
+        screen.phoneLabel.text = "Telefone: \(data.phone)"
+    }
+    
+    // MARK - Helpers
+    func setupHierarchy() {
+        addSubview(screen)
+    }
+    
+    func setupConstraints() {
+        NSLayoutConstraint.activate([
+            screen.topAnchor.constraint(equalTo: contentView.topAnchor),
+            screen.leadingAnchor.constraint(equalTo: contentView.leadingAnchor),
+            screen.trailingAnchor.constraint(equalTo: contentView.trailingAnchor),
+            screen.bottomAnchor.constraint(equalTo: contentView.bottomAnchor),
+        ])
+    }
+    
+    func setupStyle() {
+        selectionStyle = .none
+    }
+}
