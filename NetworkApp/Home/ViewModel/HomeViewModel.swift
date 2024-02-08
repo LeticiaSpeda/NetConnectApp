a30c3c7c1699ff4491a3e6ec86224401828cedbb
Leticia <leticiaspeda@gmail.com>
2024-02-08 10:37:25 -0300

    feat: adicionado funcoes para lidar com a logica da viewController

--- NetworkApp/Home/ViewModel/HomeViewModel.swift
+++ NetworkApp/Home/ViewModel/HomeViewModel.swift
@@ -1,10 +1,27 @@
 import UIKit
 
+protocol HomeViewModelDelegate: AnyObject {
+    func success()
+    func error(message: String)
+}
+
 final class HomeViewModel {
-    var service =  HomeService()
+    
+    private let service =  HomeService()
+    private var peopleList: [People] = []
+    private weak var delegate: HomeViewModelDelegate?
+    
+    var numberOfRowsInSection: Int {
+        return peopleList.count
+    }
+    
+    func delegate(delegate: HomeViewModelDelegate) {
+        self.delegate = delegate
+    }
     
     func fetchRequest() { // metodo para disparar reqscao
-        service.getPersonList { result in
+        service.getPersonList { [weak self] result in
+            guard let self else { return }
             switch result {
             case .success(let success):
                 print(success)
@@ -12,5 +29,9 @@ final class HomeViewModel {
                 print(failure)
             }
         }
+    }
+    
+    func loadCurrentPeople(indexPath: IndexPath) -> People {
+        return peopleList[indexPath.row]
     }
 }
