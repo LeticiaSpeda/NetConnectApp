26e715cb9cdc281c2aac10668f7b0e1b7a791040
Leticia <leticiaspeda@gmail.com>
2024-02-08 10:38:35 -0300

    feat: adicionado view para customizacao da tableViewCell

--- NetworkApp/CustomTableViewCellScreen.swift
+++ NetworkApp/CustomTableViewCellScreen.swift
@@ -1,8 +1,59 @@
-//
-//  CustomTableViewCellScreen.swift
-//  NetworkApp
-//
-//  Created by Leticia Speda on 08/02/24.
-//
+import UIKit
 
-import Foundation
+final class CustomTableViewCellScreen: UIView, ViewCode {
+    
+    lazy var nameLabel: UILabel = {
+        let label = UILabel()
+        label.textColor = .black
+        label.font = .boldSystemFont(ofSize: 16)
+        label.enableViewCode()
+        return label
+    }()
+    
+    lazy var surnameLabel: UILabel = {
+        let label = UILabel()
+        label.textColor = .black
+        label.font = .boldSystemFont(ofSize: 16)
+        label.enableViewCode()
+        return label
+    }()
+    
+    lazy var phoneLabel: UILabel = {
+        let label = UILabel()
+        label.textColor = .black
+        label.font = .boldSystemFont(ofSize: 16)
+        label.enableViewCode()
+        return label
+    }()
+    
+    override init(frame: CGRect) {
+        super.init(frame: .zero)
+        commonInit()
+    }
+    
+    @available(*, unavailable)
+    required init?(coder: NSCoder) { nil }
+    
+    func setupHierarchy() {
+        addSubview(nameLabel)
+        addSubview(surnameLabel)
+        addSubview(phoneLabel)
+    }
+    
+    func setupConstraints() {
+        NSLayoutConstraint.activate([
+            nameLabel.topAnchor.constraint(equalTo: safeAreaLayoutGuide.topAnchor, constant: 10),
+            nameLabel.leadingAnchor.constraint(equalTo: safeAreaLayoutGuide.leadingAnchor, constant: 10),
+            nameLabel.trailingAnchor.constraint(equalTo: safeAreaLayoutGuide.trailingAnchor, constant: -10),
+            
+            surnameLabel.topAnchor.constraint(equalTo: nameLabel.topAnchor, constant: 10),
+            surnameLabel.leadingAnchor.constraint(equalTo: safeAreaLayoutGuide.leadingAnchor, constant: 10),
+            surnameLabel.trailingAnchor.constraint(equalTo: safeAreaLayoutGuide.trailingAnchor, constant: -10),
+            
+            phoneLabel.topAnchor.constraint(equalTo: surnameLabel.topAnchor, constant: 10),
+            phoneLabel.leadingAnchor.constraint(equalTo: safeAreaLayoutGuide.leadingAnchor, constant: 10),
+            phoneLabel.trailingAnchor.constraint(equalTo: safeAreaLayoutGuide.trailingAnchor, constant: -10),
+        ])
+    }
+}
+
