0a4cef44581f292b95c99e65eba7c9b2ce4fb68c
Leticia <leticiaspeda@gmail.com>
2024-02-09 08:31:17 -0300

    refactor: ajustando o codigo

--- NetworkApp/Service/ServiceManager.swift
+++ NetworkApp/Service/ServiceManager.swift
@@ -1,14 +1,6 @@
 import Foundation
 
-enum HTTPMethod: String {
-    case get = "GET"
-    case post = "POST"
-    case put = "PUT"
-    case delete = "DELETE"
-}
-
 protocol NetworkLayer {
-    var session: URLSession { get }
     func request<T: Decodable>(with urlString: String, method: HTTPMethod, decodeType: T.Type, completion: @escaping (Result<T, NetworkError>) -> Void)
 }
 
@@ -30,32 +22,32 @@ final class ServiceManager: NetworkLayer {
         
         let task = session.dataTask(with: request) { data, response, error in
             
-                if let error {
-                    print("ERROR \(#function) Detalhe do erro: \(error.localizedDescription)")
-                    completion(.failure(.networkFailure(error)))
-                    return
-                }
-                
-                guard let data else {
-                    completion(.failure(.noData))
-                    return
-                }
-                
-                guard let response = response as? HTTPURLResponse, 200...299 ~= response.statusCode else {
-                    completion(.failure(.invalidResponse))
-                    return
-                }
-                
-                do {
-                    let decoder = JSONDecoder()
-                    decoder.dateDecodingStrategy = .iso8601
-                    let object: T = try decoder.decode(T.self, from: data)
-                    print("SUCCESS -> \(#function)")
-                    completion(.success(object))
-                } catch  {
-                    print("ERROR -> \(#function)")
-                    completion(.failure(.decodingError(error)))
-                }
+            if let error {
+                print("ERROR \(#function) Detalhe do erro: \(error.localizedDescription)")
+                completion(.failure(.networkFailure(error)))
+                return
+            }
+            
+            guard let data else {
+                completion(.failure(.noData))
+                return
+            }
+            
+            guard let response = response as? HTTPURLResponse, 200...299 ~= response.statusCode else {
+                completion(.failure(.invalidResponse))
+                return
+            }
+            
+            do {
+                let decoder = JSONDecoder()
+                decoder.dateDecodingStrategy = .iso8601
+                let object: T = try decoder.decode(T.self, from: data)
+                print("SUCCESS -> \(#function)")
+                completion(.success(object))
+            } catch  {
+                print("ERROR -> \(#function)")
+                completion(.failure(.decodingError(error)))
+            }
         }
         task.resume()
     }
